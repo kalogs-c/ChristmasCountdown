d3aa65156103bf832cafe7a44d7459068d9db247
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-05-12 17:25:12 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const daysDiv = document.querySelector('.days')
+const hoursDiv = document.querySelector('.hours')
+const minutesDiv = document.querySelector('.minutes')
+const secondsDiv = document.querySelector('.seconds')
+
+const christmas = '25 Dec 2021'
+
+const formatTime = time => {
+    return time < 10 ? `0${time}` : time
+}
+
+const countdown = () => {
+    const christmasDate = new Date(christmas)
+    const currentDate = new Date()
+
+    const totalSeconds = (christmasDate - currentDate) / 1000
+
+    const seconds = Math.round(totalSeconds) % 60
+    const minutes = Math.round(totalSeconds / 60) % 60
+    const hours = Math.round(totalSeconds / 3600) % 24
+    const days = Math.round(totalSeconds / 3600 / 24)
+
+    daysDiv.textContent = days
+    hoursDiv.textContent = formatTime(hours)
+    minutesDiv.textContent = formatTime(minutes)
+    secondsDiv.textContent = formatTime(seconds)
+}
+countdown()
+setInterval(countdown, 1000)
